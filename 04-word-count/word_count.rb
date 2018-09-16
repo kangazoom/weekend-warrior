739333ead8c376d5183a5f2d97ab3702faa77f91
kangazoom <michellelcronin@gmail.com>
2018-09-16 04:32:28 -0700

    i remember doing this in think python, and the solution came to me quickly: a rare event indeed

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,16 @@
 
+def words(string)
+  words = string.split(" ")
+
+  word_freq_hash = {}
+
+  words.each do |word|
+    if word_freq_hash.keys.include? word
+      word_freq_hash[word] += 1
+    else
+      word_freq_hash[word] = 1
+    end
+  end
+
+  return word_freq_hash
+end
